a13d884a2a5201a613a5d1ace06897ad6a1da7a9
ahmetdurur <ahmet.durur@outlook.com>
2022-03-06 15:28:29 +0300

    Spring Boot WebSocket , SockJS ve Stomp Kullanarak Chat uygulaması geliştirme

---
+++ spring-boot-websocket/src/main/java/com/haydikodlayalim/ws/api/ChatController.java
@@ -0,0 +1,28 @@
+package com.haydikodlayalim.ws.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
+
+@Controller
+@CrossOrigin
+public class ChatController {
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/chat")
+    //@SendTo("/topic")
+    //@SendToUser()
+    public void chatEndPoint(@Payload WsMessage wsMessage){
+        System.out.println(wsMessage);
+        messagingTemplate.convertAndSend("/topic", wsMessage);
+    }
+}
